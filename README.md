e7ef33bbcea74b071ff80a0a186798ae25b0c97f
qeffects <qfluxstudios@gmail.com>
2021-09-15 20:18:37 +0300

    Optimizations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # A Typescript To Lua Priority list
+
+A simple implementation of a priority list with typescript declarations, uses lua-bin-search for the insertion searching as well as object pooling for a *lower* memory impact.
--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,63 @@
+import { binarySearch } from "lua-bin-search";
+import { ObjectPool } from "lua-object-pool";
+
+interface ListMember {
+    priority: number; member: any
+}
+
+function ListMemberReset(this: any, o: ListMember){
+    o.member = 0;
+    o.priority = 0;
+};
+
+function ListMemberFactory(this: any){return {priority: 0, member: 0}}
+
+const priorityPool = new ObjectPool<ListMember>(50, 100, ListMemberFactory, ListMemberReset)
+
 export class PriorityList<T> {
-    list: { priority: number; tiebreak: number; member: T }[];
+    list: { priority: number; member: T }[];
     private presence: LuaTable<T, boolean>;
     constructor() {
         this.list = [];
         this.presence = new LuaTable<T, boolean>();
     }
 
-    put(priority: number, member: T, tiebreak?: number): void {
+    /**
+     * Adds a member to this priority list
+     * @param priority the priority of the member, lower means higher priority
+     * @param member the member itself
+     */
+    put(priority: number, member: T): void {
         this.presence.set(member, true);
 
+        const listM = priorityPool.take()
+        listM.member = member;
+        listM.priority = priority;
+
         if (this.list.length < 1) {
-            table.insert(this.list, { priority: priority, tiebreak: tiebreak, member: member });
+            table.insert(this.list, listM);
         }
 
         if (this.list.length < 2) {
             if (this.list[0].priority < priority) {
-                table.insert(this.list, 2, { priority: priority, tiebreak: tiebreak, member: member });
+                table.insert(this.list, 2, listM);
             } else {
-                table.insert(this.list, 1, { priority: priority, tiebreak: tiebreak, member: member });
+                table.insert(this.list, 1, listM);
             }
         }
 
         if (this.list.length >= 2) {
-            let startindex = 1;
-            let endIndex = this.list.length;
-            let insertIndex = 1;
-            let found = false;
-            let iter = 0;
+            let insertIndex = binarySearch.findInsert(this.list, "priority", priority)
 
-            if (this.list[this.list.length - 1].priority > priority) {
-                insertIndex = this.list.length + 1;
-                found = true;
-            }
-
-            if (this.list[0].priority < priority) {
-                insertIndex = 1;
-                found = true;
-            }
-
-            while (!found) {
-                iter += 1;
-                const checkIndex = math.floor((endIndex - startindex) / 2 + startindex);
-                if (this.list[checkIndex - 1].priority > priority) {
-                    if (this.list[checkIndex]) {
-                        if (this.list[checkIndex].priority < priority) {
-                            insertIndex = checkIndex + 1;
-                            break;
-                        }
-                    } else {
-                        insertIndex = checkIndex + 1;
-                        break;
-                    }
-                    startindex = checkIndex + 1;
-                } else if (this.list[checkIndex - 1].priority < priority) {
-                    if (this.list[checkIndex - 2]) {
-                        if (this.list[checkIndex - 2].priority > priority) {
-                            insertIndex = checkIndex;
-                            break;
-                        }
-                    } else {
-                        insertIndex = checkIndex;
-                        break;
-                    }
-                    endIndex = checkIndex - 1;
-                } else {
-                    if (this.list[checkIndex - 1].tiebreak && tiebreak) {
-                        if (this.list[checkIndex - 1].tiebreak > tiebreak) {
-                            insertIndex = checkIndex + 1;
-                            break;
-                        } else {
-                            insertIndex = checkIndex;
-                            break;
-                        }
-                    }
-                    insertIndex = checkIndex;
-                    break;
-                }
-
-                if (iter > 100) {
-                    error("too many iterations");
-                }
-            }
-            table.insert(this.list, insertIndex, { priority: priority, tiebreak: tiebreak, member: member });
+            table.insert(this.list, insertIndex, listM);
         }
     }
 
+    /**
+     * Finds a member in the priority list
+     * @param member The member to be found
+     * @returns Position of the member
+     */
     find(member: T): number {
         for (const i of $range(1, this.list.length)) {
             if (this.list[i - 1].member === member) {
@@ -94,24 +67,39 @@ export class PriorityList<T> {
         return 1;
     }
 
+    /**
+     * Removes a member from the priority list
+     * @param member The member to be removed
+     */
     remove(member: T): void {
         const i = this.find(member);
+        const lm = table.remove(this.list, i)
 
         this.presence.delete(member);
 
-        table.remove(this.list, i);
+        lm && priorityPool.free(lm);
     }
 
+    /**
+     * Checks if the member is in the priority list
+     * @param member 
+     * @returns True if it's present, false if not
+     */
     isPresent(member: T): boolean {
         return this.presence.has(member);
     }
 
+    /**
+     * Removes and returns the member with the highest priority
+     * @returns your highest priority member
+     */
     take(): T {
         const c = this.list[this.list.length - 1].member;
-
         this.presence.delete(c);
 
-        table.remove(this.list, this.list.length);
+        const lm = table.remove(this.list, this.list.length);
+
+        lm && priorityPool.free(lm);
 
         return c;
     }
